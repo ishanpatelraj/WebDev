93e53d772523e83fc74fbbbfe446c3c5fc038138
Ishan Patel <ishanpatelraj@gmail.com>
2026-03-14 16:12:49 +0530

    Rename person.js to Person.js and fix requires

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Person = require('../models/Person');
-const { model } = require('mongoose');
 
 // Create a new person
 router.get('/:workType', async (req, res) => {
